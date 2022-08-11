36b2bca6a84aeecfdb1939452c8f87243c6f9fe0
Laurentiu Molnar <lau.molnar2013@gmail.com>
2022-08-11 18:59:33 +0300

    Update deploy workflow

--- src/books-service/index.ts
+++ src/books-service/index.ts
@@ -3,6 +3,6 @@ import type { APIGatewayProxyResultV2 } from 'aws-lambda';
 export async function handler(): Promise<APIGatewayProxyResultV2> {
   return {
     statusCode: 200,
-    body: JSON.stringify('hello'),
+    body: JSON.stringify('hello, world!'),
   };
 }
